875c99373328408c8613f6dd0b5a5b3e7759e9be
Boxking776 <64418882+Boxking776@users.noreply.github.com>
2022-02-27 08:50:35 +0000

    Update loadperks.lua

--- functions/premium/loadperks.lua
+++ functions/premium/loadperks.lua
@@ -6,7 +6,7 @@ function loadPremium(Window)
   print("Version "..tostring(ver))
   local premtab = Window:CreateTab("Premium")
   local test = premtab:CreateSection("Testing")
-  test:CreateToggle("Autofarm", nil, function(State) --[[getgenv().kocmoc.toggles.autofarm = State]] end)
+  test:CreateToggle("Autofarm", nil, function(State) getgenv().kocmoc.toggles.autofarm = State end)
 end
 
 return loadPremium
