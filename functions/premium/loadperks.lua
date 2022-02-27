116188d653a011edc226b6dbf0b31624fdbc5e76
Boxking776 <64418882+Boxking776@users.noreply.github.com>
2022-02-27 08:19:42 +0000

    Update loadperks.lua

--- functions/premium/loadperks.lua
+++ functions/premium/loadperks.lua
@@ -3,6 +3,7 @@ function loadPremium(Window)
   if Window ~= nil then print("library passed") end
   print("ok2")
   local premtab = Window:CreateTab("Premium")
+  local autofarmtoggle = premtab:CreateToggle("Autofarm", nil, function(State) getgenv().kocmoc.toggles.autofarm = State end)
 end
 
 return loadPremium
