a6e26bc9d597d3ee5cb0abef073f95d841e1e9a3
Boxking776 <64418882+Boxking776@users.noreply.github.com>
2022-02-27 14:36:37 +0000

    Update loadperks.lua

--- functions/premium/loadperks.lua
+++ functions/premium/loadperks.lua
@@ -1,12 +1,23 @@
-local ver = 5
+local ver = 6
+local ASSET_ID = 8958348861
+ 
+local Players = game:GetService("Players")
+local MarketplaceService = game:GetService("MarketplaceService")
+local PlayerOwnsAsset = MarketplaceService.PlayerOwnsAsset
+local player = game:GetService("Players").LocalPlayer
+
+local function isPremium()
+local success, doesPlayerOwnAsset = pcall(PlayerOwnsAsset, MarketplaceService, player, ASSET_ID)
+return doesPlayerOwnAsset
+end
 
 function loadPremium(Window)
   print("requested load")
   if Window ~= nil then print("library passed") end
   print("Version "..tostring(ver))
   local premtab = Window:CreateTab("Premium")
-  local test = premtab:CreateSection("Testing")
-  test:CreateToggle("Autofarm", nil, function(State) getgenv().kocmoc.toggles.autofarm = State end)
+  local info = premtab:CreateSection("Info")
+  if isPremium() then info:CreateLabel("You are a Premium user.") else info:CreateLabel("You are not a Premium user.") end
 end
 
 return loadPremium
