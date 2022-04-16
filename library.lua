af6ae4bad1865bc7f3c8f98a92cb8ed7623244cf
Boxking776 <64418882+Boxking776@users.noreply.github.com>
2022-04-16 10:53:27 +0100

    Update library.lua

--- library.lua
+++ library.lua
@@ -254,6 +254,12 @@ function Library:CreateWindow(Config, Parent)
 				Button.Parent = Section.Container
 				Button.Title.Text = Name
 				Button.Size = UDim2.new(1,-10,0,Button.Title.TextBounds.Y + 5)
+				
+				function ButtonInit:UpdateText(Text)
+					Button.Text = Text
+					Button.Size = UDim2.new(1,-10,0,Button.Title.TextBounds.Y + 5)
+				end
+				
 				table.insert(Library.ColorTable, Button)
 
 				Button.MouseButton1Down:Connect(function()
