476df7cb34f9db23ec10f461870e110da99707aa
Boxking776 <64418882+Boxking776@users.noreply.github.com>
2022-05-29 18:23:15 +0100

    Update kocmoc-remastered.lua

--- kocmoc-remastered.lua
+++ kocmoc-remastered.lua
@@ -19,7 +19,7 @@ Thanks for using kocmoc! ^-^
 
 if game.PlaceId == 1537690962 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Boxking776/kocmoc/main/games/bss.lua"))()
-else
+elseif game.PlaceId == 6284583030 or game.PlaceId == 7722306047 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Boxking776/kocmoc/main/games/psx.lua"))()
 else
 	game.Players.LocalPlayer:Kick("Error loadng kocmoc.\n\nNo supported game found.\n\nPlease join a supported game.")
